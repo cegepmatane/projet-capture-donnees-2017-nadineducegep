a2980fff6a03f077c27a84b9ba3090399bfd943b
nadineducegep <st-amand.nadine@cgmatane.qc.ca>
2017-10-23 09:00:35 -0400

    Affichage balises <mouton>

--- services-de-donnees/service.bergerie/mouton/liste/index.php
+++ services-de-donnees/service.bergerie/mouton/liste/index.php
@@ -1,10 +1,24 @@
 <?php 
-	include "../../basededonnees.php";
 
+	// LECTURE des données
+	include "../../basededonnees.php";
 	$SQL_LISTE_MOUTON = "SELECT * FROM mouton";
 	$requeteListeMoutons = $basededonnees->prepare($SQL_LISTE_MOUTON);
 	$resultat = $requeteListeMoutons->execute();
 	$listeMoutons = $requeteListeMoutons->fetchAll();
 	//print_r($listeMoutons);
 ?>
-coucou
+<?php
+	// AFFICHAGE des données
+	foreach($listeMoutons as $mouton)
+	{
+		//print_r($mouton);
+		?>
+		<mouton>
+			<id><?=$mouton['id_mouton']?></id>
+			<nom><?=$mouton['nom']?></nom>
+			<description><?=$mouton['description']?></description>
+		</mouton>
+		<?php
+	}
+?>
